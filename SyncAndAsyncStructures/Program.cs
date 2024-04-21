2ea61561165dbf5f02ce7b260763646911011023
Vinicius Miranda <jvptech@outlook.com>
2024-04-21 09:49:18 -0400

    Add project files.

---
+++ SyncAndAsyncStructures/Program.cs
@@ -0,0 +1,27 @@
+﻿using HttpClientSyncStructure;
+using System.Net.Http.Json;
+
+HttpClient? client = new();
+HttpRequestMessage request = new(HttpMethod.Get, "https://jsonplaceholder.typicode.com/todos");
+HttpResponseMessage? reponse = client.Send(request);
+
+if(reponse.IsSuccessStatusCode)
+{
+    List<Todo>? todoList = reponse.Content.ReadFromJsonAsync<List<Todo>>().Result;
+
+    if(todoList is not null)
+    {
+        foreach(Todo todos in todoList)
+        {
+            Console.WriteLine($"id: {todos.Id}, title: {todos.Title}, completed: {todos.Completed}, userId: {todos.UserId}");
+        }
+    }
+    else
+    {
+        Console.WriteLine("The list is empty!");
+    }
+}
+else
+{
+    Console.Write($"HTTP ERROR: {reponse.StatusCode}");
+}
---
+++ SyncAndAsyncStructures/Todo.cs
@@ -0,0 +1,10 @@
+﻿namespace HttpClientSyncStructure
+{
+    public class Todo
+    {
+        public int UserId { get; set; }
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public bool Completed { get; set; }
+    }
+}
